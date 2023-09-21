fbc345b8dc11ea5bd61441739c21492621d7e9e2
jiyoon <vywns9978@gmail.com>
2023-09-21 19:53:04 +0900

    [🍡 style] 스타일드 컴포넌트 및 테마 적용 테스트

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Login() {
   const [user, setUser] = useState("");
@@ -13,10 +14,16 @@ function Login() {
 
   return (
     <div>
-      <h1>Login</h1>
+      <LoginHeaderText>Login</LoginHeaderText>
       {user ? <h1>user: {user}</h1> : <h1>로그인 x</h1>}
     </div>
   );
 }
 
 export default Login;
+
+const LoginHeaderText = styled.h1`
+  background-color: ${({ theme }) => theme.color.emotion_1};
+  color: ${({ theme }) => theme.color.white};
+  padding-left: 10px;
+`;

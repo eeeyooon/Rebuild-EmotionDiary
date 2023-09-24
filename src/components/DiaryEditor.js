0b5bfaae6ebc5e2dc8f8dfb80560eebdeee43a48
jiyoon <vywns9978@gmail.com>
2023-09-24 16:27:37 +0900

    [✨ feat] 일기 작성, 수정, 삭제 시 커스텀 모달 적용

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -7,6 +7,7 @@ import { emotionList } from "../utils/emotion.js";
 import MyButton from "./MyButton";
 import MyHeader from "./MyHeader";
 import EmotionItem from "./EmotionItem";
+import Modal from "./Modal";
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
@@ -14,11 +15,18 @@ const DiaryEditor = ({ isEdit, originData }) => {
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
   const navigate = useNavigate();
+  const [openModal, setOpenModal] = useState(false);
+  const [modalStatus, setModalStatus] = useState("");
+  const [modalCheck, setModalCheck] = useState(false);
 
   const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
   const handleClickEmote = useCallback((emotion) => {
     setEmotion(emotion);
   }, []);
+
+  const handleModal = (modalState) => {
+    setOpenModal(modalState);
+  };
 
   const handleSubmit = () => {
     if (content.legth < 1) {
@@ -26,24 +34,33 @@ const DiaryEditor = ({ isEdit, originData }) => {
       return;
     }
 
-    if (
-      window.confirm(
-        isEdit ? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?"
-      )
-    ) {
+    if (modalCheck) {
       if (!isEdit) {
         onCreate(date, content, emotion);
+        setModalCheck(false);
       } else {
         onEdit(originData.diaryId, date, content, emotion);
+        setModalCheck(false);
       }
     }
     navigate("/home", { replace: true });
   };
 
+  useEffect(() => {
+    if (modalCheck) {
+      if (modalStatus === "수정" || modalStatus === "작성") {
+        handleSubmit();
+      } else if (modalStatus === "삭제") {
+        handleRemove();
+      }
+    }
+  }, [modalCheck, modalStatus]);
+
   const handleRemove = () => {
-    if (window.confirm("정말 삭제하시겠습니까?")) {
+    if (modalCheck) {
       onRemove(originData.diaryId);
       navigate("/home", { replace: true });
+      setModalCheck(false);
     }
   };
 
@@ -56,68 +73,89 @@ const DiaryEditor = ({ isEdit, originData }) => {
   }, [isEdit, originData]);
 
   return (
-    <div className="DiaryEditor">
-      <MyHeader
-        headText={isEdit ? "일기 수정하기" : "새로운 일기쓰기"}
-        leftChild={
-          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
-        }
-        rightChild={
-          isEdit && (
+    <>
+      {openModal && (
+        <div className="Modal_Background" onClick={() => setOpenModal(false)} />
+      )}
+      <div className="DiaryEditor">
+        <MyHeader
+          headText={isEdit ? "일기 수정하기" : "새로운 일기쓰기"}
+          leftChild={
+            <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+          }
+          rightChild={
+            isEdit && (
+              <MyButton
+                text={"삭제하기"}
+                type={"negative"}
+                onClick={() => {
+                  // handleRemove();
+                  setModalStatus("삭제");
+                  setOpenModal(true);
+                }}
+              />
+            )
+          }
+        />
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input_box">
+            <input
+              className="input_date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                isSelected={it.emotion_id === emotion}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className="input_box text_wrapper">
+            <textarea
+              placeholder="오늘은 어땠나요?"
+              ref={contentRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+        </section>
+        <section>
+          <div className="control_box">
+            <MyButton text={"취소하기"} onClick={() => navigate(-1)} />
             <MyButton
-              text={"삭제하기"}
-              type={"negative"}
-              onClick={handleRemove}
+              text={isEdit ? "수정완료" : "작성완료"}
+              type={"positive"}
+              onClick={() => {
+                setOpenModal(true);
+                setModalStatus(isEdit ? "수정" : "작성");
+              }}
             />
-          )
-        }
-      />
-      <section>
-        <h4>오늘은 언제인가요?</h4>
-        <div className="input_box">
-          <input
-            className="input_date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            type="date"
-          />
-        </div>
-      </section>
-      <section>
-        <h4>오늘의 감정</h4>
-        <div className="input_box emotion_list_wrapper">
-          {emotionList.map((it) => (
-            <EmotionItem
-              key={it.emotion_id}
-              {...it}
-              onClick={handleClickEmote}
-              isSelected={it.emotion_id === emotion}
+          </div>
+        </section>
+        {openModal && (
+          <div className="Modal_Box">
+            <Modal
+              handleModal={handleModal}
+              modalStatus={modalStatus}
+              setModalCheck={setModalCheck}
             />
-          ))}
-        </div>
-      </section>
-      <section>
-        <h4>오늘의 일기</h4>
-        <div className="input_box text_wrapper">
-          <textarea
-            placeholder="오늘은 어땠나요?"
-            ref={contentRef}
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </div>
-      </section>
-      <section>
-        <div className="control_box">
-          <MyButton text={"취소하기"} onClick={() => navigate(-1)} />
-          <MyButton
-            text={isEdit ? "수정완료" : "작성완료"}
-            type={"positive"}
-            onClick={handleSubmit}
-          />
-        </div>
-      </section>
-    </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
